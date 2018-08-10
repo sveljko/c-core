d0f7d81c53eea1c6bab15a66a3bc30f7517cf328
Petar Saric <petar.saricsa@gmail.com>
2018-08-10 11:27:13 +0200

    cleaning some diagnostic printf()-s from 'lib/sockets/pbpal_resolv_and_connect_sockets.c'

--- lib/sockets/pbpal_resolv_and_connect_sockets.c
+++ lib/sockets/pbpal_resolv_and_connect_sockets.c
@@ -174,9 +174,6 @@ enum pbpal_resolv_n_connect_result pbpal_check_resolv_and_connect(pubnub_t *pb)
     memset(&dest, '\0', sizeof dest);
     switch (read_dns_response(skt, (struct sockaddr*)&dns_server, &dest)) {
     case -1:
-//
-        printf("--------------->puko sam kod read_dns_response!\n");
-		//
 		return pbpal_resolv_failed_rcv;
     case +1:
         return pbpal_resolv_rcv_wouldblock;
