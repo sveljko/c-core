54ef42a76c94e05f470024a27ffa43e8c147d7ab
Petar Saric <petar.saricsa@gmail.com>
2019-01-23 19:53:42 +0100

    building on windows

--- lib/sockets/pbpal_adns_sockets.c
+++ lib/sockets/pbpal_adns_sockets.c
@@ -35,10 +35,10 @@
 #endif
 
 
-int send_dns_query(int skt,
+int send_dns_query(int                    skt,
                    struct sockaddr const* dest,
-                   char const* host,
-                   enum DNSqueryType query_type)
+                   char const*            host,
+                   enum DNSqueryType      query_type)
 {
     uint8_t buf[4096];
     int     to_send;
@@ -46,7 +46,7 @@ int send_dns_query(int skt,
     size_t  sockaddr_size;
 
     switch (dest->sa_family) {
-    case AF_INET: 
+    case AF_INET:
         sockaddr_size = sizeof(struct sockaddr_in);
         break;
     case AF_INET6:
@@ -60,11 +60,12 @@ int send_dns_query(int skt,
         return -1;
     }
     if (-1 == pbdns_prepare_dns_request(buf, sizeof buf, host, &to_send, query_type)) {
-        PUBNUB_LOG_ERROR("Couldn't prepare dns request! : #prepared bytes=%d\n", to_send);
+        PUBNUB_LOG_ERROR("Couldn't prepare dns request! : #prepared bytes=%d\n",
+                         to_send);
         return -1;
     }
     TRACE_SOCKADDR("Sending DNS query to: ", dest, sockaddr_size);
-    sent_to       = sendto(skt, (char*)buf, to_send, 0, dest, sockaddr_size);
+    sent_to = sendto(skt, (char*)buf, to_send, 0, dest, sockaddr_size);
     if (sent_to <= 0) {
         return socket_would_block() ? +1 : -1;
     }
@@ -85,7 +86,7 @@ int read_dns_response(int skt, struct sockaddr* dest, struct sockaddr** resolved
     enum DNSqueryType         addr_type;
 
     switch (dest->sa_family) {
-    case AF_INET: 
+    case AF_INET:
         sockaddr_size = sizeof(struct sockaddr_in);
         break;
     case AF_INET6:
@@ -103,33 +104,34 @@ int read_dns_response(int skt, struct sockaddr* dest, struct sockaddr** resolved
         return socket_would_block() ? +1 : -1;
     }
 
-    if (pbdns_pick_resolved_address(buf, (size_t)msg_size, &temp_resolved_addr, &addr_type) != 0) {
+    if (pbdns_pick_resolved_address(buf, (size_t)msg_size, &temp_resolved_addr, &addr_type)
+        != 0) {
         return -1;
     }
     switch (addr_type) {
-    case dnsA:
-        {
-            /* This memory must be liberated(freed) in 'connecting' function */
-            struct sockaddr_in* p_addr_st = (struct sockaddr_in*)malloc(sizeof(struct sockaddr_in));
-            
-            memcpy(&(p_addr_st->sin_addr.s_addr),
-                   &(temp_resolved_addr.ipv4),
-                   sizeof(struct pubnub_ipv4_address));
-            *resolved_addr = (struct sockaddr*)p_addr_st;
-            (*resolved_addr)->sa_family = AF_INET;
-        }
-        return 0;
-    case dnsAAAA:
-        {
-            /* This memory must be liberated(freed) in 'connecting' function */
-            struct sockaddr_in6* p_addr_st = (struct sockaddr_in6*)malloc(sizeof(struct sockaddr_in6));
+    case dnsA: {
+        /* This memory must be liberated(freed) in 'connecting' function */
+        struct sockaddr_in* p_addr_st =
+            (struct sockaddr_in*)malloc(sizeof(struct sockaddr_in));
 
-            memcpy(p_addr_st->sin6_addr.s6_addr,
-                   &(temp_resolved_addr.ipv6),
-                   sizeof(struct pubnub_ipv6_address));
-            *resolved_addr = (struct sockaddr*)p_addr_st;
-            (*resolved_addr)->sa_family = AF_INET6;
-        }
+        memcpy(&(p_addr_st->sin_addr.s_addr),
+               &(temp_resolved_addr.ipv4),
+               sizeof(struct pubnub_ipv4_address));
+        *resolved_addr              = (struct sockaddr*)p_addr_st;
+        (*resolved_addr)->sa_family = AF_INET;
+    }
+        return 0;
+    case dnsAAAA: {
+        /* This memory must be liberated(freed) in 'connecting' function */
+        struct sockaddr_in6* p_addr_st =
+            (struct sockaddr_in6*)malloc(sizeof(struct sockaddr_in6));
+
+        memcpy(p_addr_st->sin6_addr.s6_addr,
+               &(temp_resolved_addr.ipv6),
+               sizeof(struct pubnub_ipv6_address));
+        *resolved_addr              = (struct sockaddr*)p_addr_st;
+        (*resolved_addr)->sa_family = AF_INET6;
+    }
         return 0;
     default:
         PUBNUB_LOG_ERROR("read_dns_response(socket=%d): invalid address type "
@@ -143,29 +145,45 @@ int read_dns_response(int skt, struct sockaddr* dest, struct sockaddr** resolved
 
 //#if 0
 #include <stdio.h>
+#if !defined(_WIN32)
 #include <fcntl.h>
+#endif
 int main()
 {
-    struct sockaddr_in dest;
+    struct sockaddr_in  dest;
     struct sockaddr_in6 dest6;
+    struct sockaddr*    resolved_addr;
 
+#if defined(_WIN32)
+    WSADATA wsaData;
+    int iResult;
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
+#endif
     puts("===========================ADNS-AF_INET========================");
 
     int skt = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+#if !defined(_WIN32)
     int flags = fcntl(skt, F_GETFL, 0);
     fcntl(skt, F_SETFL, flags | O_NONBLOCK);
-
-    dest.sin_family = AF_INET;
-    dest.sin_port = htons(53);
-    dest.sin_addr.s_addr = inet_addr("208.67.222.222");
+#endif
+    dest.sin_family      = AF_INET;
+    dest.sin_port        = htons(53);
+    inet_pton(AF_INET, "208.67.222.222", &(dest.sin_addr.s_addr));
 
     if (-1 == send_dns_query(skt, (struct sockaddr*)&dest, "facebook.com", dnsANY)) {
         PUBNUB_LOG_ERROR("Error: Couldn't send datagram(Ipv4).\n");
         return -1;
     }
 
-    fd_set read_set;
-    int rslt;
+#if !defined(_WIN32)
+    fd_set         read_set;
+    int            rslt;
     struct timeval timev = { 0, 300000 };
 
     FD_ZERO(&read_set);
@@ -176,32 +194,40 @@ int main()
         return -1;
     }
     else if (rslt > 0) {
-        struct sockaddr* resolved_addr;
-        printf("skt=%d, rslt=%d, timev.tv_sec=%ld, timev.tv_usec=%ld\n", skt, rslt, timev.tv_sec, timev.tv_usec);
+        printf("skt=%d, rslt=%d, timev.tv_sec=%ld, timev.tv_usec=%ld\n",
+               skt,
+               rslt,
+               timev.tv_sec,
+               timev.tv_usec);
+#endif
         read_dns_response(skt, (struct sockaddr*)&dest, &resolved_addr);
+#if !defined(_WIN32)
     }
     else {
         puts("no select() event(Ipv4).");
     }
+#endif
     socket_close(skt);
 
     puts("=========================ADNS-AF_INET6========================");
 
-    skt = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+    skt   = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+#if !defined(_WIN32)
     flags = fcntl(skt, F_GETFL, 0);
     fcntl(skt, F_SETFL, flags | O_NONBLOCK);
-    timev.tv_sec = 0;
+    timev.tv_sec  = 0;
     timev.tv_usec = 300000;
-
+#endif
     dest6.sin6_family = AF_INET6;
-    dest6.sin6_port = htons(53);
-    inet_pton(AF_INET6, "2620:119:35::35", &dest6.sin6_addr);
+    dest6.sin6_port   = htons(53);
+    inet_pton(AF_INET6, "2620:119:35::35", dest6.sin6_addr.s6_addr);
 
     if (-1 == send_dns_query(skt, (struct sockaddr*)&dest6, "facebook.com", dnsANY)) {
         PUBNUB_LOG_ERROR("Error: Couldn't send datagram(Ipv6).\n");
         return -1;
     }
 
+#if !defined(_WIN32)
     FD_ZERO(&read_set);
     FD_SET(skt, &read_set);
     rslt = select(skt + 1, &read_set, NULL, NULL, &timev);
@@ -210,13 +236,19 @@ int main()
         return -1;
     }
     else if (rslt > 0) {
-        struct sockaddr* resolved_addr;
-        printf("skt=%d, rslt=%d, timev.tv_sec=%ld, timev.tv_usec=%ld\n", skt, rslt, timev.tv_sec, timev.tv_usec);
+        printf("skt=%d, rslt=%d, timev.tv_sec=%ld, timev.tv_usec=%ld\n",
+               skt,
+               rslt,
+               timev.tv_sec,
+               timev.tv_usec);
+#endif
         read_dns_response(skt, (struct sockaddr*)&dest6, &resolved_addr);
+#if !defined(_WIN32)
     }
     else {
         puts("no select() event(Ipv6).");
     }
+#endif
     socket_close(skt);
 
     return 0;
