2268d154ae6e484f94f5e435d6134294f3787912
Srdjan Veljkovic <sveljko@gvs.rs>
2019-10-18 13:24:19 +0200

    Bring back set/get connection timeout
    It got lost in a bad merge

--- cpp/pubnub_common.hpp
+++ cpp/pubnub_common.hpp
@@ -1535,16 +1535,38 @@ public:
         std::chrono::milliseconds result(pubnub_transaction_timeout_get(d_pb));
         return result;
     }
+    /// Sets the connection timeout duration	
+    int set_connection_timeout(std::chrono::milliseconds duration)	
+    {	
+        return pubnub_set_wait_connect_timeout(d_pb, duration.count());	
+    }	
+    /// Returns the connection timeout duration	
+    std::chrono::milliseconds connection_timeout_get()	
+    {	
+        std::chrono::milliseconds result(pubnub_wait_connect_timeout_get(d_pb));	
+        return result;	
+    }	
 #else
-    /// Returns the transaction timeout duration
+    /// Returns the transaction timeout duration, in milliseconds
     int set_transaction_timeout(int duration_ms)
     {
         return pubnub_set_transaction_timeout(d_pb, duration_ms);
     }
+    /// Returns the transaciton timeout duration, in milliseconds
     int transaction_timeout_get()
     {
         return pubnub_transaction_timeout_get(d_pb);
     }
+    /// Sets the connection timeout duration, in milliseconds	
+    int set_connection_timeout(int duration_ms)	
+    {	
+        return pubnub_set_wait_connect_timeout(d_pb, duration_ms);	
+    }	
+    /// Returns the connection timeout duration, in milliseconds
+    int connection_timeout_get()	
+    {	
+        return pubnub_wait_connect_timeout_get(d_pb);	
+    }	
 #endif
 
     /// Frees the context and any other thing that needs to be
